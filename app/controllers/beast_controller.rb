e880e6da8dc6648557677387fc35341a4ddfbba5
Neo <sergio.yanez@alumnos.usm.cl>
2018-08-06 00:47:16 -0700

    Reading beasts from Contract

--- app/controllers/beast_controller.rb
+++ app/controllers/beast_controller.rb
@@ -1,27 +1,56 @@
 class BeastController < ApplicationController
+
+  def get_web3
+    web3 = Web3::Eth::Rpc.new host: ENV['INFURA_ENDPOINT'], 
+              port: 443,  
+              connect_options: {
+              open_timeout: 20,
+              read_timeout: 140,
+              use_ssl: true,
+              rpc_path: '/'+ENV['INFURA_API_KEY']
+            }
+    return web3
+  end
+
   def get_information
+
+  	if params[:id].blank?
+  		raise ActiveRecord::RecordNotFound, "Address not found."
+  	end
 
   	id = params[:id]
 
-  	beast_mock = {
-	    :race => 0,
-	    :genes => ["151035040544608783460", 
-	    		   "151035040544575359108", 
-	    		   "151035040475889435746", 
-	    		   "151035040475854832738", 
-	    		   "446182945655207658594", 
-	    		   "446182945655211820130"].sample,
-	    :experience => 0,
-	    :challengeCoolDown => 0,
-	    :birthTime => 1533220228,
-	    :cooldownEndBlock => 0,
-	    :sireId => 232434,
-	    :matronId => 234234,
-	    :siringWithId => 0,
-	    :cooldownIndex => 0,
-	    :generation => 0,
-	    :skillId => 0,
-	    :level => 1,
+  	web3 = get_web3
+
+    contract_info = Contract.where("name": "BEASTS").first
+    if contract_info.blank?
+      raise ActiveRecord::RecordNotFound, "Can't find contract."
+    end
+
+    beastsContract = web3.eth.contract(JSON.parse(contract_info.abi))
+
+    beasts = beastsContract.at(contract_info.address)
+
+    # Attribute list format
+    #  uint race, bool isGestating, bool isReady, uint256 cooldownEndBlock, bool isReadyToFight, uint256 experience, 
+    #  uint256 birthTime, uint256 sireId, uint256 matronId, uint256 siringWithId, uint256 cooldownIndex, uint256 generation, 
+    #  uint256 genes 
+    beast_attribute_list = beasts.getLegend(id.to_i)
+
+  	beast_structure = {
+	    :race => beast_attribute_list[0],
+	    :genes => beast_attribute_list[12],
+	    :experience => beast_attribute_list[5],
+	    :isGestating => beast_attribute_list[1],
+	    :birthTime => beast_attribute_list[6],
+	    :cooldownEndBlock => beast_attribute_list[3],
+	    :sireId => beast_attribute_list[7],
+	    :matronId => beast_attribute_list[8],
+	    :siringWithId => beast_attribute_list[9],
+	    :cooldownIndex => beast_attribute_list[10],
+	    :generation => beast_attribute_list[11],
+	    :isReady => beast_attribute_list[2],
+	    :isReadyToFight => beast_attribute_list[4],
 	    :preferedAttribute => 0,
 	    :element => 0,
 	    :pedigree => 1,
@@ -35,7 +64,7 @@ class BeastController < ApplicationController
 	    }
 	}
 
-	render plain: beast_mock.to_json
+	render plain: beast_structure.to_json
 
   end
 end
